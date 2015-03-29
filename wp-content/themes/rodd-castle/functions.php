cc4c1f2a6539a1f18bb47673f3a263a1f51215bc
Rodd <rodd@roddcastle.com>
2015-03-29 15:16:45 +1300

    First Commit

---
+++ wp-content/themes/rodd-castle/functions.php
@@ -0,0 +1,318 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'agency', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'parallax' ) );
+
+//* Add Image upload to WordPress Theme Customizer
+add_action( 'customize_register', 'parallax_customizer' );
+function parallax_customizer(){
+
+	require_once( get_stylesheet_directory() . '/lib/customize.php' );
+	
+}
+
+//* Include Section Image CSS
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Parallax Pro Theme' );
+define( 'CHILD_THEME_URL', 'http://my.studiopress.com/themes/parallax/' );
+define( 'CHILD_THEME_VERSION', '1.0' );
+
+//* Enqueue scripts and styles
+add_action( 'wp_enqueue_scripts', 'parallax_enqueue_scripts_styles' );
+function parallax_enqueue_scripts_styles() {
+
+	wp_enqueue_script( 'parallax-responsive-menu', get_bloginfo( 'stylesheet_directory' ) . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0' );
+	wp_enqueue_style( 'dashicons' );
+	wp_enqueue_style( 'parallax-google-fonts', '//fonts.googleapis.com/css?family=Montserrat|Sorts+Mill+Goudy', array(), CHILD_THEME_VERSION );
+
+}
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5' );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 1 );
+
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_nav' );
+
+//* Reposition the secondary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_footer', 'genesis_do_subnav', 7 );
+
+//* Reduce the secondary navigation menu to one level depth
+add_filter( 'wp_nav_menu_args', 'parallax_secondary_menu_args' );
+function parallax_secondary_menu_args( $args ){
+
+	if( 'secondary' != $args['theme_location'] )
+	return $args;
+
+	$args['depth'] = 1;
+	return $args;
+
+}
+
+//* Unregister layout settings
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
+
+//* Add support for additional color styles
+add_theme_support( 'genesis-style-selector', array(
+	'parallax-pro-blue'   => __( 'Parallax Pro Blue', 'parallax' ),
+	'parallax-pro-green'  => __( 'Parallax Pro Green', 'parallax' ),
+	'parallax-pro-orange' => __( 'Parallax Pro Orange', 'parallax' ),
+	'parallax-pro-pink'   => __( 'Parallax Pro Pink', 'parallax' ),
+) );
+
+//* Unregister secondary sidebar
+unregister_sidebar( 'sidebar-alt' );
+
+//* Add support for custom header
+add_theme_support( 'custom-header', array(
+	'width'           => 360,
+	'height'          => 70,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+) );
+
+//* Add support for structural wraps
+add_theme_support( 'genesis-structural-wraps', array(
+	'header',
+	'nav',
+	'subnav',
+	'footer-widgets',
+	'footer',
+) );
+
+
+class RC_About_Me_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		parent::__construct(
+			'about_me_widget', // Base ID
+			__( 'About Me', 'text_domain' ), // Name
+			array( 'description' => __( 'A widget for displaying my about me info', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		$widget_id = $args['widget_id'];
+		echo $args['before_widget'];
+		if ( get_field('profile_image', 'widget_' . $widget_id) ) {
+			$profile_image = get_field('profile_image', 'widget_' . $widget_id);
+			//print_r($profile_image);
+			echo '<img id="profile_image" src="'.$profile_image['url'].'" alt="'.$profile_image['title'].'">';
+		}
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		if ( get_field('bio', 'widget_' . $widget_id) ) {
+			the_field('bio', 'widget_' . $widget_id);
+		}
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+}
+
+// register RC_About_Me_Widget widget
+function register_rc_about_me_widget() {
+    register_widget( 'RC_About_Me_Widget' );
+}
+add_action( 'widgets_init', 'register_rc_about_me_widget' );
+
+
+class RC_Current_Highlights_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		parent::__construct(
+			'current_highlights_widget', // Base ID
+			__( 'Current Highlights', 'text_domain' ), // Name
+			array( 'description' => __( 'A widget for displaying my current highlights', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		$widget_id = $args['widget_id'];
+
+		echo $args['before_widget'];
+
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+
+		if ( get_field('bio', 'widget_' . $widget_id) ) {
+			the_field('bio', 'widget_' . $widget_id);
+		}
+
+		if(get_field('highlight', 'widget_' . $widget_id)) {
+			while(the_repeater_field('highlight', 'widget_' . $widget_id)) {
+				the_sub_field('content', 'widget_' . $widget_id);
+			}
+		}
+
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+}
+
+// register RC_About_Me_Widget widget
+function register_rc_current_highlights_widget() {
+    register_widget( 'RC_Current_Highlights_Widget' );
+}
+add_action( 'widgets_init', 'register_rc_current_highlights_widget' );
+
+//* Hook after post widget after the entry content
+add_action( 'genesis_after_entry', 'parallax_after_entry', 5 );
+function parallax_after_entry() {
+
+	if ( is_singular( 'post' ) )
+		genesis_widget_area( 'after-entry', array(
+			'before' => '<div class="after-entry widget-area">',
+			'after'  => '</div>',
+		) );
+
+}
+
+//* Modify the size of the Gravatar in the author box
+add_filter( 'genesis_author_box_gravatar_size', 'parallax_author_box_gravatar' );
+function parallax_author_box_gravatar( $size ) {
+
+	return 176;
+
+}
+
+//* Modify the size of the Gravatar in the entry comments
+add_filter( 'genesis_comment_list_args', 'parallax_comments_gravatar' );
+function parallax_comments_gravatar( $args ) {
+
+	$args['avatar_size'] = 120;
+
+	return $args;
+
+}
+
+/**
+ *
+ * Modify the size of the Gravatar in the User Profile Widget
+ *
+ * @author Calvin Koepke
+ * @since 1.0.0
+*/
+add_filter( 'genesis_gravatar_sizes', 'rc_user_profile' );
+function rc_user_profile( $sizes ) {
+	$sizes['Extra Large'] = 250;
+	return $sizes;
+}
+
+//* Register widget areas
+genesis_register_sidebar( array(
+	'id'          => 'home-section-1',
+	'name'        => __( 'Home Section 1', 'parallax' ),
+	'description' => __( 'This is the home section 1 section.', 'parallax' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'home-section-2',
+	'name'        => __( 'Home Section 2', 'parallax' ),
+	'description' => __( 'This is the home section 2 section.', 'parallax' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'home-section-3',
+	'name'        => __( 'Home Section 3', 'parallax' ),
+	'description' => __( 'This is the home section 3 section.', 'parallax' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'home-section-4',
+	'name'        => __( 'Home Section 4', 'parallax' ),
+	'description' => __( 'This is the home section 4 section.', 'parallax' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'home-section-5',
+	'name'        => __( 'Home Section 5', 'parallax' ),
+	'description' => __( 'This is the home section 5 section.', 'parallax' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'after-entry',
+	'name'        => __( 'After Entry', 'parallax' ),
+	'description' => __( 'This is the after entry widget area.', 'parallax' ),
+) );
---
+++ wp-content/themes/rodd-castle/js/parallax.js
@@ -0,0 +1,29 @@
+jQuery(function( $ ){
+
+	// Enable parallax and fade effects on homepage sections
+	$(window).scroll(function(){
+
+		scrolltop = $(window).scrollTop()
+		scrollwindow = scrolltop + $(window).height();
+
+		$(".home-section-1").css("backgroundPosition", "0px " + -(scrolltop/6) + "px");
+
+		// if( scrollwindow > $(".home-section-3").offset().top ) {
+
+		// 	// Enable parallax effect
+		// 	backgroundscroll = scrollwindow - $(".home-section-3").offset().top;
+		// 	$(".home-section-3").css("backgroundPosition", "0px " + -(backgroundscroll/6) + "px");
+
+		// }
+
+		if( scrollwindow > $(".home-section-5").offset().top ) {
+
+			// Enable parallax effect
+			backgroundscroll = scrollwindow - $(".home-section-5").offset().top;
+			$(".home-section-5").css("backgroundPosition", "0px " + -(backgroundscroll/6) + "px");
+
+		}
+
+	});
+
+});
